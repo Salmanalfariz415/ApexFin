c066c6fa94bfd1fb5a0dea50fbd8bb2067a3bb0d
Salmanalfariz-techz <Salmanalfariz332@gmail.com>
2025-07-25 18:51:21 +0530

    Added a file to store constants

---
+++ constants.js
@@ -0,0 +1,7 @@
+const constants={
+    VALIDATION_ERROR : 400,
+    UNAUTHORIZED : 401,
+    FORBIDDEN : 403,
+    NOT_FOUND : 404,
+}
+module.exports = constants;
---
+++ middleware/errorHander.js
@@ -0,0 +1,35 @@
+const code = require('constants.js');
+const statusCode =res.statusCode?res.statusCode : 500;
+const errorHandler = (err,req,res,next)=>{
+    switch(statusCode){
+        case code.VALIDATION_ERROR:
+        res.json({
+                title:"Invalid input",
+                message:err.message,
+                stackTrace :err.stack,
+        });
+        break;
+        case code.UNAUTHORIZED:
+        res.json({
+                title:"Unauthorized access",
+                message:err.message,
+                stackTrace :err.stack,
+        });
+        break;
+        case code.FORBIDDEN:
+        res.json({
+                title:"Payment required to continue",
+                message:err.message,
+                stackTrace :err.stack,
+        });
+        break;
+        case code.NOT_FOUND:
+        res.json({
+                title:"Resource not found",
+                message:err.message,
+                stackTrace :err.stack,
+        });
+        break;
+    }
+}
+module.exports=errorHandler;
