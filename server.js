59ea495c4d7d9d674a078effec08b806a4dbf587
Salmanalfariz-techz <Salmanalfariz332@gmail.com>
2025-07-30 23:43:25 +0530

    feat: add global rate limiter middleware using express-rate-limit

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path=require('path');
 const app = express();
 app.use(express.json());
+const { globalLimiter } = require('./middleware/limiter');
+app.use(globalLimiter);
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'public','dashboard','dashboard.html'));
