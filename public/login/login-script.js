64ec95b2948300952fa721a56ab64e3fa1d67ccb
Salmanalfariz-techz <Salmanalfariz332@gmail.com>
2025-08-01 15:37:09 +0530

    Defined and designed the login page

--- public/login/login-script.js
+++ public/login/login-script.js
@@ -1,3 +1,43 @@
 if (performance.navigation.type === 1) {
-    window.location.href = "/";
+    window.location.href = "/dashboard";
 }
+let email=document.getElementById("email");
+let pass=document.getElementById("password");
+let submit=document.getElementById("submit");
+let userEmail;
+let passwd;
+let response=document.getElementById("response");
+
+async function sendEmail(userEmail,passwd){
+    try{
+        const result = await fetch('/login/send',{
+        method:'POST',
+        headers: {
+                'Content-Type': 'application/json'
+        },
+        body : JSON.stringify({
+            email:userEmail,
+            password:passwd,
+            
+        })
+    })
+    const data=await result.json();
+        return{success:result.ok,data:data};
+    }
+    catch(err){
+        return{error:err.message,success:false};
+
+    }
+}
+loginsubmit.addEventListener("click",async function(){
+    userEmail=email.value.trim;
+    passwd=pass.value.trim;
+    sendEmail(userEmail,passwd);
+    const result=await sendEmail(userEmail,passwd);
+    if(result.success){
+        response.innerHTML="Successfully Logged In"
+    }
+    else{
+        response.innerHTML="Invalid Credentails"
+    }
+});
